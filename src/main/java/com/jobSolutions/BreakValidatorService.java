6331e58fcc81ab6f1dd9d036f757b94eaca2f612
Mat191126 <matprz11@gmail.com>
2021-09-23 11:28:00 +0200

    Add models, service and controller classes

---
+++ src/main/java/com/jobSolutions/BreakValidatorService.java
@@ -0,0 +1,8 @@
+package com.jobSolutions;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BreakValidatorService {
+
+}
---
+++ src/main/java/com/jobSolutions/model/Break.java
@@ -0,0 +1,8 @@
+package com.jobSolutions.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Break {
+
+}
