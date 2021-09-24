01bba1f5ceec285de8aa6028f933027be5295b02
Mat191126 <matprz11@gmail.com>
2021-09-24 15:03:50 +0200

    Add method testing Break setters

--- src/test/java/com/jobSolutions/model/BreakTest.java
+++ src/test/java/com/jobSolutions/model/BreakTest.java
@@ -8,7 +8,32 @@ import org.junit.jupiter.api.Test;
 class BreakTest {
 
     @Test
-    void constructor_ReturnBreakWithFilledFromAndUntilFields_WhenSettersNotUsed() {
+    void constructor_ReturnBreakWithFilledFromField_WhenFromSetterNotUsed() {
+        //Arrange
+        Break expected = new Break();
+        expected.setPaidBreakTime(0, 15)
+                .setRequiredWorkingTime(7, 0)
+                .setFrom(DateTime.now().minusYears(2000))
+                .setUntil(2020, 5, 12);
+        //Act
+        Break actual = new Break();
+        actual.setPaidBreakTime(0, 15)
+                .setRequiredWorkingTime(7, 0)
+                .setUntil(2020, 5, 12);
+        //Assert
+        assertAll(
+                () -> assertEquals(expected.getPaidBreakTime(), actual.getPaidBreakTime()
+                                    , "Break object fields are different"),
+                () -> assertEquals(expected.getRequiredWorkingTime(), actual.getRequiredWorkingTime()
+                                    , "Break object fields are different"),
+                () -> assertEquals(expected.getUntil(), actual.getUntil(), "Break object fields are different"),
+                () -> assertNotEquals(null, actual.getFrom(), "From field can't be null"),
+                () -> assertNotEquals(null, actual.getUntil(), "Until field can't be null")
+        );
+    }
+
+    @Test
+    void constructor_ReturnBreakWithFilledUntilField_WhenUntilSetterNotUsed() {
         //Arrange
         Break expected = new Break();
         expected.setPaidBreakTime(0, 15)
@@ -22,10 +47,13 @@ class BreakTest {
                 .setFrom(2020, 11, 5);
         //Assert
         assertAll(
-                () -> assertEquals(expected.getPaidBreakTime(), actual.getPaidBreakTime()),
-                () -> assertEquals(expected.getRequiredWorkingTime(), actual.getRequiredWorkingTime()),
-                () -> assertEquals(expected.getFrom(), actual.getFrom()),
-                () -> assertEquals(expected.getUntil(), actual.getUntil())
+                () -> assertEquals(expected.getPaidBreakTime(), actual.getPaidBreakTime()
+                                    , "Break object fields are different"),
+                () -> assertEquals(expected.getRequiredWorkingTime(), actual.getRequiredWorkingTime()
+                                    , "Break object fields are different"),
+                () -> assertEquals(expected.getFrom(), actual.getFrom(), "Break object fields are different"),
+                () -> assertNotEquals(null, actual.getFrom(), "From field can't be null"),
+                () -> assertNotEquals(null, actual.getUntil(), "Until field can't be null")
         );
     }
 }
