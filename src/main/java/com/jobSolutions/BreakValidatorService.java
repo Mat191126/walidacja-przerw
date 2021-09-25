bc383c87f61a1f15c0e852d3cc9209dd2dad2022
Mat191126 <matprz11@gmail.com>
2021-09-25 13:43:59 +0200

    BreakValidatorService refactor of printing info

--- src/main/java/com/jobSolutions/BreakValidatorService.java
+++ src/main/java/com/jobSolutions/BreakValidatorService.java
@@ -1,14 +1,13 @@
 package com.jobSolutions;
 
-import com.jobSolutions.model.Break;
-import com.jobSolutions.model.BreakSequence;
-import com.jobSolutions.model.PresenceConfirmation;
-import com.jobSolutions.model.PresenceConfirmationSequence;
+import com.jobSolutions.model.*;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
 import org.joda.time.Interval;
 
 public class BreakValidatorService {
+    // Constant variable for temporary toggle working time check
+    private final boolean IS_WORKING_TIME_CHECKED = false;
 
     public boolean changePreviousSequence(BreakSequence previousBreakSequence, BreakSequence newBreakSequence,
                                           PresenceConfirmationSequence confirmationSequence) {
@@ -19,21 +18,41 @@ public class BreakValidatorService {
 
     private boolean checkNewSequence(BreakSequence newBreakSequence,
                                     PresenceConfirmationSequence confirmationSequence) {
+        StringBuilder info = new StringBuilder();
+        info.append("--------- New break definition info -----------\n");
+
+        // Check break periods overlapping
         boolean isPeriodOverlap = checkIfBreakPeriodsOverlaps(newBreakSequence);
-        boolean isRequiredWorkingTimeEnough = checkIfRequiredWorkingTimeEnough(newBreakSequence);
+        info.append("Periods overlaps: ")
+                .append(isPeriodOverlap)
+                .append("\n");
+
+        //Optionally check if required working time is at least 6h
+        boolean isRequiredWorkingTimeEnough = true;
+        if (IS_WORKING_TIME_CHECKED) {
+            isRequiredWorkingTimeEnough = checkIfRequiredWorkingTimeEnough(newBreakSequence);
+            info.append("Is required working time enough: ")
+                    .append(isRequiredWorkingTimeEnough)
+                    .append("\n");
+        }
+        info.append("Is required working time enough: not required\n");
+
+        // Check if there is no presence confirmation set during the new break definition period
         boolean hasPresenceConfirmation = checkIfPresenceConfirmationCollideBreakPeriod(newBreakSequence,
                                                                                         confirmationSequence);
-        System.out.println("---------info-----------");
-        System.out.println("Periods overlaps: " + isPeriodOverlap);
-        System.out.println("Is required working time enough: " + isRequiredWorkingTimeEnough);
-        System.out.println("Break have presence confirmation: " + hasPresenceConfirmation);
-        return true;
+        info.append("Break have presence confirmation: ")
+                .append(hasPresenceConfirmation)
+                .append("\n")
+                .append("-----------------------------------------------");
+
+        System.out.println(info.toString());
+        return !isPeriodOverlap && isRequiredWorkingTimeEnough && !hasPresenceConfirmation;
     }
 
     public boolean checkIfBreakPeriodsOverlaps(BreakSequence newBreakSequence) {
         for (Break singleBreak : newBreakSequence.getBreakSequence()) {
             Interval singleBreakInterval = new Interval(singleBreak.getFrom(), singleBreak.getUntil());
-            System.out.println(singleBreak.toString());
+//            System.out.println(singleBreak.toString());
             for (Break singleBreakToCompare :  newBreakSequence.getBreakSequence()) {
                 if (!singleBreak.equals(singleBreakToCompare)) {
                     Interval singleBreakToCompareInterval = new Interval(singleBreakToCompare.getFrom(),
@@ -62,7 +81,7 @@ public class BreakValidatorService {
             Interval singleBreakInterval = new Interval(singleBreak.getFrom(), singleBreak.getUntil());
             for (PresenceConfirmation presenceConfirmation :  presenceSequence.getPresenceConfirmationSequence()) {
                 DateTime presenceDate = presenceConfirmation.getDate();
-                System.out.println(presenceConfirmation.toString());
+//                System.out.println(presenceConfirmation.toString());
                 if (singleBreakInterval.contains(presenceDate)) {
                     return true;
                 }
